682ff55711891e5bfbbe5a14ff548a18e26acc3b
Angelo Buenaventura <64779950+Champupoy@users.noreply.github.com>
2021-06-24 14:28:27 +0800

    Add files via upload
    todo list using the boxed layout for bootstrap

--- blank.php
+++ blank.php
@@ -41,6 +41,69 @@
     <!-- Main content -->
     <div class="content">
       <!-- PLACE YOUR PAGE CONTENTS HERE -->
+      <?php
+          $servername = "localhost";
+          $username = "id16885056_sympu_todolist";
+          $password = "9&%?njtn$)HDiz}Q";
+          $dbname = "id16885056_sympu_todolist_db";
+
+          $conn = new mysqli($servername, $username, $password, $dbname);
+
+          if ($conn -> connect_error) 
+          {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+          $sql = 'SELECT name, description, time_allotment, date_assigned, source FROM todo';
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0)
+          {
+            while($row = $result->fetch_assoc())
+            {
+      
+      ?>        
+
+      <div class="card">
+        <div class="card-header">
+          <h2 class="card-title"><?php echo $row ['name']; ?> &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;  <h7>Date Assigned: </h7> <?php echo $row ['date_assigned']; ?> </h2>
+
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                <i class="fas fa-times"></i>
+              </button>
+            </div>
+        </div>
+
+        <div class="card-body">
+          <h7>Instruction(s): </h7> <?php echo $row ['description']; ?> <br>
+          <h7>Time Allotment: </h7> <?php echo $row ['time_allotment']; ?> minutes<br>
+        </div>
+          <!-- /.card-body -->
+
+        <div class="card-footer">
+          <h7>Source: </h7> <?php echo $row ['source']; ?> 
+        </div>
+        <!-- /.card-footer-->
+
+      </div>
+      <!-- /.card -->
+
+        
+
+      <?php    
+        }
+      }
+
+        $conn->close();
+      ?>
+
+        <br>
+
+      
       <div class="container-fluid">
         
       </div><!-- /.container-fluid -->
